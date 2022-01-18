4b082d9759e02b9a96580ab9fe43e007d0344aea
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:47:25 +0300

    pom update

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,26 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer;
+
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
